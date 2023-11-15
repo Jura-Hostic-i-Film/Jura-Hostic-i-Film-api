07c4b9f80c992ceafd8239dcf8cd5a830e3d2301
Jura Hostic <jura.hostic@gmail.com>
2023-11-15 11:13:20 +0100

    Split auditor into multiple roles

--- app/utils/enums.py
+++ app/utils/enums.py
@@ -4,6 +4,8 @@ from enum import Enum
 class RolesEnum(str, Enum):
     ADMIN = 'admin'
     DIRECTOR = 'director'
-    AUDITOR = 'auditor'
+    AUDITOR_RECEIPT = 'auditor_recepit'
+    AUDITOR_OFFER = 'auditor_offer'
+    AUDITOR_INTERNAL = 'auditor_internal'
     ACCOUNTANT = 'accountant'
     EMPLOYEE = 'employee'

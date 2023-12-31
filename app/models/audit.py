517b24f336346c7d04cc19b6f2d0b3a8e326990d
Sulphuratum2 <karlo.grga13@gmail.com>
2023-12-31 18:02:07 +0100

    Ispravak modela

--- app/models/audit.py
+++ app/models/audit.py
@@ -1,22 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.sqltypes import DateTime
 
 from app.config.database import Base
-
-audit_users = Table(
-    "audit_users",
-    Base.metadata,
-    Column("audit_id", Integer, ForeignKey("audit.audit_id")),
-    Column("user_id", Integer, ForeignKey("users.id")),
-)
-
-audit_documents = Table(
-    "audit_documents",
-    Base.metadata,
-    Column("audit_id", Integer, ForeignKey("audit.audit_id")),
-    Column("document_id", Integer, ForeignKey("documents.id")),
-)
 
 class AuditDB(Base):
     __tablename__ = "audit"
@@ -24,5 +9,7 @@ class AuditDB(Base):
     audit_id = Column(Integer, primary_key=True, index=True)
     audited_at = Column(DateTime)
     status = Column(String)
-    audited_by = relationship("UserDB", secondary=audit_users, back_populates="audits", cascade="all, delete")
-    document_id = relationship("DocumentDB", secondary=audit_documents, back_populates="audits", cascade="all, delete")
+    audited_by = Column(Integer, ForeignKey("users.id"))
+    document_id = Column(Integer, ForeignKey('documents.id'))
+    audited = relationship("UserDB", back_populates="audits", single_parent=True)
+    document = relationship("DocumentDB", back_populates="audits", single_parent=True)
